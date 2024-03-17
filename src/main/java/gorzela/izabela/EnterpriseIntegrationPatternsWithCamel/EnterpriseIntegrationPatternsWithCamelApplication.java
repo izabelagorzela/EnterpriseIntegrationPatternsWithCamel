a0b919cdc16d378f6a15752c65a9d55561638dec
Izabela Gorzela <izabela.m.gorzela@gmail.com>
2024-03-17 18:57:19 +0100

    Empty Spring Boot project created

---
+++ src/main/java/gorzela/izabela/EnterpriseIntegrationPatternsWithCamel/EnterpriseIntegrationPatternsWithCamelApplication.java
@@ -0,0 +1,13 @@
+package gorzela.izabela.EnterpriseIntegrationPatternsWithCamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseIntegrationPatternsWithCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseIntegrationPatternsWithCamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gorzela/izabela/EnterpriseIntegrationPatternsWithCamel/EnterpriseIntegrationPatternsWithCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package gorzela.izabela.EnterpriseIntegrationPatternsWithCamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseIntegrationPatternsWithCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
